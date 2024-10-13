e3dd3f87935a32a7b0568d54976ca16c8cfc19af
fay <167489527+necloid@users.noreply.github.com>
2024-10-13 14:36:48 +0700

    Add files via upload

---
+++ Tugas1/ball.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+class Bola{
+
+public:
+    Point posisi;
+    int dx, dy;
+    Scalar warna;
+    int radius;
+
+    Bola(Point posAwal, int kecepatanX, int kecepatanY, Scalar warnaBola, int radiusBola)
+        : posisi(posAwal), dx(kecepatanX), dy(kecepatanY), warna(warnaBola), radius(radiusBola){}
+
+    void move(int frameWidth, int frameHeight){
+
+        posisi.x += dx;
+        posisi.y += dy;
+
+        if(posisi.y >= frameHeight || posisi.y <= 0) dy *= -1;
+        if(posisi.x >= frameWidth || posisi.x <= 0) dx *= -1;
+
+    }
+
+    void draw(Mat &frame)const{
+
+        circle(frame, posisi, radius, warna, -1);
+
+    }
+
+    void ubahWarna(){
+
+        warna = Scalar(rand() % 256, rand() % 256, rand() % 256);
+
+    }
+};
---
+++ Tugas1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include "ball.hpp"
+#include "game.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(){
+
+    Program game;
+    game.execute();
+    return 0;
+
+}
