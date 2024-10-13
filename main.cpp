23b18bb83851669decc24d83546e2acec9c9d530
fay <167489527+necloid@users.noreply.github.com>
2024-10-13 13:52:08 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include "ball.hpp"
+#include "game.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(){
+
+    Game game;
+    game.run();
+    return 0;
+
+}
