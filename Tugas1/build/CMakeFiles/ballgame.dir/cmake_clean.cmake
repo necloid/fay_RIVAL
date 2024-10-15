77d033743b0a765a8b0c26a202cf84552fa984e4
fay <167489527+necloid@users.noreply.github.com>
2024-10-15 08:42:58 +0700

    Add files via upload

---
+++ Tugas1/build/CMakeFiles/ballgame.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ballgame.dir/src/main.cpp.o"
+  "ballgame"
+  "ballgame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ballgame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tugas1/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+#include "ball.hpp"
+#include "game.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(){
+
+    Program gameProgram;
+    gameProgram.execute();
+    
+    return 0;
+
+}
